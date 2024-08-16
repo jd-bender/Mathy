e9607ded68eaf6ac8aee64b4ac6cc35bfa757135
Jake Bender <jdbender64@gmail.com>
2024-08-15 20:27:22 -0700

    adds second page to addition 1 lesson

--- src/app/(authenticated)/arithmetic/addition-1/Lesson.tsx
+++ src/app/(authenticated)/arithmetic/addition-1/Lesson.tsx
@@ -1,13 +1,15 @@
 import { useState } from "react";
 import { Box, Button, Typography } from "@mui/material";
 import { Transition } from "@headlessui/react";
-import { page1Content } from "./LessonContent";
+import { page1Content, page2Content } from "./LessonContent";
+import NumberLine from "app/components/NumberLine/NumberLine";
 
 export default function Lesson() {
     const [lessonStarted, setLessonStarted] = useState(false);
     const [startButtonDisabled, setStartButtonDisabled] = useState(false);
     const [pageLoading, setPageLoading] = useState(false);
-    const [page1, setPage1] = useState([
+    const [hideActivePageContent, setHideActivePageContent] = useState(false);
+    const [activePageContent, setActivePageContent] = useState([
         <Transition show={true} key={0}></Transition>,
     ]);
 
@@ -15,13 +17,17 @@ export default function Lesson() {
         setStartButtonDisabled(true);
         setLessonStarted(true);
 
+        populatePage1Content();
+    };
+
+    const populatePage1Content = () => {
         page1Content.textContent.forEach((content, index) => {
             setTimeout(() => {
                 if (index === 0) {
                     setPageLoading(true);
                 }
 
-                setPage1((currentPage1) => [
+                setActivePageContent((currentPage1) => [
                     ...currentPage1,
                     <Transition show={true} appear={true} key={index + 1}>
                         <Typography
@@ -37,8 +43,8 @@ export default function Lesson() {
 
         setTimeout(
             () => {
-                setPage1((currentPage1) => [
-                    ...currentPage1,
+                setActivePageContent((currentActivePageContent) => [
+                    ...currentActivePageContent,
                     <Transition
                         show={true}
                         appear={true}
@@ -60,11 +66,57 @@ export default function Lesson() {
         );
     };
 
-    const advanceToNextLessonPage = () => {};
+    const populatePage2Content = () => {
+        setActivePageContent((currentActivePageContent) => [
+            ...currentActivePageContent,
+            <Transition show={true} appear={true} key={1}>
+                <NumberLine
+                    range={[0, 10]}
+                    explanationMode={true}
+                    startPosition={3}
+                    endPosition={8}
+                    modifier={5}
+                />
+            </Transition>,
+        ]);
+
+        page2Content.textContent.forEach((content, index) => {
+            setTimeout(() => {
+                setActivePageContent((currentActivePageContent) => [
+                    ...currentActivePageContent,
+                    <Transition show={true} appear={true} key={index + 2}>
+                        <Typography
+                            mb={2}
+                            className={`transition-opacity ease-in-out duration-1000 data-[closed]:opacity-0`}
+                        >
+                            {content}
+                        </Typography>
+                    </Transition>,
+                ]);
+            }, page2Content.timeoutDurations[index]);
+        });
+    };
+
+    const advanceToNextLessonPage = () => {
+        setHideActivePageContent(true);
+
+        setTimeout(() => {
+            setActivePageContent([
+                <Transition show={true} key={0}></Transition>,
+            ]);
+            setHideActivePageContent(false);
+            populatePage2Content();
+        }, 1000);
+    };
 
     return (
         <Box className="relative">
-            {page1}
+            <span
+                className={`${hideActivePageContent ? "opacity-0" : "opacity-100"} transition-opacity ease-in-out duration-1000`}
+            >
+                {activePageContent}
+            </span>
+
             <Button
                 className={`${lessonStarted ? "opacity-0" : "opacity-100 absolute top-0 z-10"} ${pageLoading && "hidden"} transition-opacity ease-in-out duration-1000`}
                 onClick={startLesson}
--- src/app/(authenticated)/arithmetic/addition-1/LessonContent.tsx
+++ src/app/(authenticated)/arithmetic/addition-1/LessonContent.tsx
@@ -5,5 +5,17 @@ export const page1Content = {
         "This course will begin with simple arithmetic, and will gradually escalate all the way into multivariable calculus, linear algebra, and differential equations. It will be a long journey, but with a little time, effort and determination, you'll be a mathematics professional before you know it!",
         "Our journey into the world of math begins with this first step, so let's get started!",
     ],
-    timeoutDurations: [1000, 4000, 10000, 18000, 21000],
+    // timeoutDurations: [1000, 4000, 10000, 18000, 21000],
+    timeoutDurations: [0, 0, 0, 0, 0],
+};
+
+export const page2Content = {
+    textContent: [
+        "This is called a number line. We can use it to count up or down from a starting position.",
+        "Let's start with an example of how we can use this number line to solve a simple addition problem.",
+        "3 + 5 = ?",
+        "First, we should find our starting position, 3, on the number line.",
+        "You can use the mouse cursor to hover over each position and click to select them. The dot will be light green to show it can be selected, and then dark green once you click to select it.",
+    ],
+    timeoutDurations: [1000, 4000, 7000, 9000, 12000, 17000],
 };
