4172ab835b426722a2802e4d4936ce6e7814c058
Jake Bender <jdbender64@gmail.com>
2024-08-30 05:43:08 -0700

    adds query param to track lesson page

--- src/app/components/LessonLayout.tsx
+++ src/app/components/LessonLayout.tsx
@@ -1,18 +1,49 @@
 "use client";
-import { ReactNode, useState } from "react";
+import { ReactNode, useState, useMemo, useCallback, useEffect } from "react";
 import { Box, Button } from "@mui/material";
 import TabPanel from "app/components/TabPanel";
+import { useRouter, usePathname, useSearchParams } from "next/navigation";
 
 export default function LessonLayout({ pages }) {
-    const [activePageIndex, setActivePageIndex] = useState(0);
+    const router = useRouter();
+    const pathname = usePathname();
+    const searchParams = useSearchParams();
+    const params = useMemo(
+        () => new URLSearchParams(searchParams),
+        [searchParams],
+    );
+
+    const setParam = useCallback(
+        (name: string, value: string) => {
+            params.set(name, value);
+            router.push(pathname + "?" + params.toString());
+        },
+        [params, pathname, router],
+    );
+
+    let activePage = 0;
+
+    if (params.get("page")) {
+        activePage = Number(params.get("page"));
+    }
+
+    useEffect(() => {
+        if (!params.get("page")) {
+            setParam("page", "0");
+        }
+    }, [params, setParam]);
+
+    const [activePageIndex, setActivePageIndex] = useState(activePage);
 
     const goToLastPage = () => {
+        setParam("page", String(activePageIndex - 1));
         setActivePageIndex(
             (oldActivePageIndex: number) => oldActivePageIndex - 1,
         );
     };
 
     const goToNextPage = () => {
+        setParam("page", String(activePageIndex + 1));
         setActivePageIndex(
             (oldActivePageIndex: number) => oldActivePageIndex + 1,
         );
