3e7eb396177b63870119c1a37189c661f5b5dc7b
Jake Bender <jdbender64@gmail.com>
2024-08-30 03:31:34 -0700

    adds module layout component

--- src/app/(authenticated)/page.tsx
+++ src/app/(authenticated)/page.tsx
@@ -6,12 +6,19 @@ export default function Page() {
     const router = useRouter();
 
     return (
-        <div className="p-12 mx-48">
+        <div className="p-12 mx-48 flex justify-evenly">
             <Button
                 variant="contained"
                 onClick={() => router.push("/arithmetic/addition-1")}
             >
                 Addition 1
+            </Button>
+
+            <Button
+                variant="contained"
+                onClick={() => router.push("/arithmetic/subtraction-1")}
+            >
+                Subtraction 1
             </Button>
         </div>
     );
---
+++ src/app/components/ModuleLayout.tsx
@@ -0,0 +1,50 @@
+"use client";
+import { useState, SyntheticEvent, ReactNode } from "react";
+import { Tab, Box } from "@mui/material";
+import { TabPanel, TabList, TabContext } from "@mui/lab";
+
+export default function ModuleLayout({
+    Lesson,
+    Practice,
+}: {
+    Lesson: ReactNode;
+    Practice: ReactNode;
+}) {
+    const [selectedTab, setSelectedTab] = useState("0");
+
+    const handleSelectedTabChange = (
+        _event: SyntheticEvent,
+        newTab: string,
+    ) => {
+        setSelectedTab(newTab);
+    };
+
+    return (
+        <Box sx={{ width: "100%" }}>
+            <TabContext value={selectedTab}>
+                <Box
+                    sx={{
+                        borderBottom: 1,
+                        borderColor: "divider",
+                        display: "flex",
+                        justifyContent: "center",
+                    }}
+                >
+                    <TabList onChange={handleSelectedTabChange}>
+                        <Tab label="Lesson" value="0" />
+                        <Tab label="Practice" value="1" />
+                    </TabList>
+                </Box>
+
+                <Box className="relative flex flex-col grow p-12 mx-96 m-w-96">
+                    <TabPanel value="0" keepMounted>
+                        {Lesson}
+                    </TabPanel>
+                    <TabPanel value="1" keepMounted>
+                        {Practice}
+                    </TabPanel>
+                </Box>
+            </TabContext>
+        </Box>
+    );
+}
