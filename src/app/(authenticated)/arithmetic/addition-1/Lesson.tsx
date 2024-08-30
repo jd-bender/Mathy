4559d07610a0f3cacdf118d8faf07556f67ef130
Jake Bender <jdbender64@gmail.com>
2024-08-30 01:49:29 -0700

    cleans up code

--- src/app/(authenticated)/arithmetic/addition-1/Lesson.tsx
+++ src/app/(authenticated)/arithmetic/addition-1/Lesson.tsx
@@ -1,12 +1,9 @@
-import { useState } from "react";
 import { Box, Button, Typography } from "@mui/material";
 import TabPanel from "app/components/TabPanel";
 import { pages } from "./LessonContent";
 import NumberLine from "app/components/NumberLine/NumberLine";
 
-export default function Lesson() {
-    const [activePageIndex, setActivePageIndex] = useState(0);
-
+export default function Lesson({ activePageIndex, setActivePageIndex }) {
     const goToLastPage = () => {
         setActivePageIndex((oldActivePageIndex) => oldActivePageIndex - 1);
     };
@@ -15,63 +12,67 @@ export default function Lesson() {
         setActivePageIndex((oldActivePageIndex) => oldActivePageIndex + 1);
     };
 
-    return (
-        <Box className="relative flex flex-col grow p-12 mx-96 m-w-96">
-            <TabPanel value={activePageIndex} index={0}>
-                {pages[0].map((content, index) => (
+    const PlainTextMapping = (content: string, index: number) => (
+        <Typography mb={2} key={index}>
+            {content}
+        </Typography>
+    );
+
+    const Page1 = pages[0].map(PlainTextMapping);
+
+    const Page2 = pages[1].map((content, index) => {
+        switch (index) {
+            case 1:
+                return (
+                    <NumberLine
+                        range={[0, 10]}
+                        explanationMode={true}
+                        key={index}
+                        className="mb-2"
+                    />
+                );
+            case 5:
+                return (
+                    <NumberLine
+                        range={[0, 10]}
+                        explanationMode={true}
+                        startPosition={3}
+                        key={index}
+                        className="mb-2"
+                    />
+                );
+            case 7:
+                return (
+                    <NumberLine
+                        range={[0, 10]}
+                        explanationMode={true}
+                        startPosition={3}
+                        endPosition={8}
+                        key={index}
+                        className="mb-2"
+                    />
+                );
+            default:
+                return (
                     <Typography mb={2} key={index}>
                         {content}
                     </Typography>
-                ))}
+                );
+        }
+    });
+
+    const Page3 = pages[2].map(PlainTextMapping);
+
+    return (
+        <>
+            <TabPanel value={activePageIndex} index={0}>
+                {Page1}
             </TabPanel>
             <TabPanel value={activePageIndex} index={1}>
-                {pages[1].map((content, index) => {
-                    switch (index) {
-                        case 1:
-                            return (
-                                <NumberLine
-                                    range={[0, 10]}
-                                    explanationMode={true}
-                                    key={index}
-                                    className="mb-2"
-                                />
-                            );
-                        case 5:
-                            return (
-                                <NumberLine
-                                    range={[0, 10]}
-                                    explanationMode={true}
-                                    startPosition={3}
-                                    key={index}
-                                    className="mb-2"
-                                />
-                            );
-                        case 7:
-                            return (
-                                <NumberLine
-                                    range={[0, 10]}
-                                    explanationMode={true}
-                                    startPosition={3}
-                                    endPosition={8}
-                                    key={index}
-                                    className="mb-2"
-                                />
-                            );
-                        default:
-                            return (
-                                <Typography mb={2} key={index}>
-                                    {content}
-                                </Typography>
-                            );
-                    }
-                })}
+                {Page2}
             </TabPanel>
             <TabPanel value={activePageIndex} index={2}>
-                {pages[2].map((content, index) => (
-                    <Typography mb={2} key={index}>
-                        {content}
-                    </Typography>
-                ))}
+                {Page3}
             </TabPanel>
 
             <Box className="flex justify-between">
@@ -90,6 +91,6 @@ export default function Lesson() {
                     Next
                 </Button>
             </Box>
-        </Box>
+        </>
     );
 }
--- src/app/(authenticated)/arithmetic/addition-1/page.tsx
+++ src/app/(authenticated)/arithmetic/addition-1/page.tsx
@@ -8,6 +8,7 @@ import Practice from "./Practice";
 
 export default function Page() {
     const [selectedTab, setSelectedTab] = useState(0);
+    const [activeLessonPage, setActiveLessonPage] = useState(0);
 
     const handleSelectedTabChange = (
         _event: SyntheticEvent,
@@ -32,12 +33,17 @@ export default function Page() {
                 </Tabs>
             </Box>
 
-            <TabPanel value={selectedTab} index={0}>
-                <Lesson />
-            </TabPanel>
-            <TabPanel value={selectedTab} index={1}>
-                <Practice />
-            </TabPanel>
+            <Box className="relative flex flex-col grow p-12 mx-96 m-w-96">
+                <TabPanel value={selectedTab} index={0}>
+                    <Lesson
+                        activePageIndex={activeLessonPage}
+                        setActivePageIndex={setActiveLessonPage}
+                    />
+                </TabPanel>
+                <TabPanel value={selectedTab} index={1}>
+                    <Practice />
+                </TabPanel>
+            </Box>
         </Box>
     );
 }
