2909c35cea966825b1c0c1a92bb28307dd4bd5b1
Jake Bender <jdbender64@gmail.com>
2024-08-30 03:38:55 -0700

    removes another unneeded component

--- src/app/(authenticated)/layout.tsx
+++ src/app/(authenticated)/layout.tsx
@@ -1,12 +1,14 @@
 import { ReactNode } from "react";
 import { Box } from "@mui/material";
-import AppHeader from "app/components/AppHeader";
+import Link from "next/link";
 
 export default function DashboardLayout({ children }: { children: ReactNode }) {
     return (
         <Box>
             <Box className="w-full h-16 flex justify-center items-center bg-palette-1">
-                <AppHeader />
+                <Link href="/" className="text-3xl font-medium">
+                    Mathy
+                </Link>
             </Box>
 
             <Box>{children}</Box>
--- src/app/components/AppHeader.tsx
+++
@@ -1,9 +0,0 @@
-import Link from "next/link";
-
-export default function AppHeader() {
-    return (
-        <Link href="/" className="text-3xl font-medium">
-            Mathy
-        </Link>
-    );
-}
