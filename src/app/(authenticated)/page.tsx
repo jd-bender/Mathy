f5f149a789aeaff566dced28e976b825f0b92bb9
Jake Bender <jdbender64@gmail.com>
2024-08-30 03:10:31 -0700

    cleans up home page

--- src/app/(authenticated)/page.tsx
+++ src/app/(authenticated)/page.tsx
@@ -1,41 +1,18 @@
 "use client";
-import { useState } from "react";
-import { List, Collapse } from "@mui/material";
-import NavButton from "../components/NavButton";
+import { Button } from "@mui/material";
 import { useRouter } from "next/navigation";
 
 export default function Page() {
-    const [arithmeticOpen, setArithmeticOpen] = useState(false);
-    const [additionOpen, setAdditionOpen] = useState(false);
-
     const router = useRouter();
 
     return (
-        <List>
-            <NavButton
-                clickAction={() => setArithmeticOpen(!arithmeticOpen)}
-                buttonText="Arithmetic"
-                controlsCollapse={true}
-                collapseOpen={arithmeticOpen}
-            />
-            <Collapse in={arithmeticOpen}>
-                <NavButton
-                    sx={{ pl: 4 }}
-                    clickAction={() => setAdditionOpen(!additionOpen)}
-                    buttonText="Addition"
-                    controlsCollapse={true}
-                    collapseOpen={additionOpen}
-                />
-                <Collapse in={additionOpen}>
-                    <NavButton
-                        sx={{ pl: 8 }}
-                        clickAction={() =>
-                            router.push("/arithmetic/addition-1")
-                        }
-                        buttonText="Addition 1"
-                    />
-                </Collapse>
-            </Collapse>
-        </List>
+        <div className="p-12 mx-48">
+            <Button
+                variant="contained"
+                onClick={() => router.push("/arithmetic/addition-1")}
+            >
+                Addition 1
+            </Button>
+        </div>
     );
 }
