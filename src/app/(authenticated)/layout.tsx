a4e6be3563e75a092e5cc83540d256629a229067
Jake Bender <jdbender64@gmail.com>
2024-09-01 10:32:49 -0700

    refactors header styling

--- src/app/(authenticated)/layout.tsx
+++ src/app/(authenticated)/layout.tsx
@@ -4,8 +4,8 @@ import Link from "next/link";
 export default function DashboardLayout({ children }: IParent) {
     return (
         <>
-            <div className="h-16 w-full text-center relative bg-palette-1">
-                <Link href="/" className="text-3xl font-medium relative top-3">
+            <div className="h-16 flex justify-center items-center bg-palette-1">
+                <Link href="/" className="text-3xl font-medium">
                     Mathy
                 </Link>
             </div>
